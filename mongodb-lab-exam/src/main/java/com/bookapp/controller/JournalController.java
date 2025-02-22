f7d1ff2484a62366e1d72ac3ec51472d09bd5800
emohapatra <Evangel.Mohapatra@koerber-supplychain.com>
2025-02-22 22:00:23 +0530

    labexam-mongodb

---
+++ mongodb-lab-exam/src/main/java/com/bookapp/controller/JournalController.java
@@ -0,0 +1,34 @@
+package com.bookapp.controller;
+
+import com.bookapp.entities.JournalEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public class JournalController {
+    private JournalService journalService;
+
+    @Autowired
+    public JournalController(JournalService journalService) {
+        this.journalService = journalService;
+    }
+
+    @GetMapping("/journal")
+    public ResponseEntity<List<JournalEntry>> getAllJournalEntries() {
+        return ResponseEntity.status(HttpStatus.OK).body(journalService.getAll());
+    }
+
+    @GetMapping("/journal/{id}")
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(journalService.getById(id));
+    }
+
+    @GetMapping("/journal/user/{id}")
+    public ResponseEntity<List<JournalEntry>> getJournalEntryByUserId(@PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(journalService.getByUserId(id));
+    }
+}
